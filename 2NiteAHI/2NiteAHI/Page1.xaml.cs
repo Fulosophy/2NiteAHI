1d043ec4bdebf80edca5fb386c0ec88c0cd5cea5
Lordodson <65096932+Lordodson@users.noreply.github.com>
2021-03-25 22:43:55 -0600

    -Merged Map MainPage with Login UIs. -Added new logo's fire is just the flame, fireName is the flame with app name. -Modified the bluetone theme with some indigo and DarkOrchid stripe to go more with the new logo. -Style's need work on the other UI's of the login. -Added a loop so we can go out to forgot password and registering and then loop back to the login page.

---
+++ 2NiteAHI/2NiteAHI/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _2NiteAHI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Page1 : ContentPage
+{
+    public Page1()
+    {
+        InitializeComponent();
+    }
+        async void ForgotButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Page2()); }
+        async void RegButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Page3()); }
+        async void SubmitButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new MainPage()); }
+    }
+}
