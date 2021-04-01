bd25f6ed7e14f500254f125f87230629353f490b
Lordodson <65096932+Lordodson@users.noreply.github.com>
2021-04-01 11:22:41 -0600

    -Added C# for users and database. -Added SQLite interface class -Added functions to register. -Ran into an issue with user/database objects in register, fixed. -Started working on Login, ran into a similar object error. -Took a break and organized the Settings Xaml.

--- 2NiteAHI/2NiteAHI/Login.xaml.cs
+++ 2NiteAHI/2NiteAHI/Login.xaml.cs
@@ -1,23 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.ComponentModel;
+using System.Windows.Input;
+using _2NiteAHI.Helper;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace _2NiteAHI
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Login : ContentPage
-{
-    public Login()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
     {
-        InitializeComponent();
-    }
+        public Login()
+        {
+            InitializeComponent();
+            //UserDatabase userData = new UserDatabase();
+            //NavigationPage.SetHasBackButton(this, false);
+
+        }
+
         async void ForgotButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Recovery()); }
         async void RegButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Register()); }
         async void SubmitButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new MainPage()); }
+
     }
 }
