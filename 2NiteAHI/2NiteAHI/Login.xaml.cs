7b5ee6058bd953f2a5739ce929f5e1f11a938901
Lordodson <65096932+Lordodson@users.noreply.github.com>
2021-04-01 23:02:04 -0600

    -Finished the Login page initial function seutp. -Getting a null error on the button clicked actions to work on. But I believe the hard part is done. -Debugging is required.

--- 2NiteAHI/2NiteAHI/Login.xaml.cs
+++ 2NiteAHI/2NiteAHI/Login.xaml.cs
@@ -13,14 +13,51 @@ namespace _2NiteAHI
         public Login()
         {
             InitializeComponent();
-            //UserDatabase userData = new UserDatabase();
-            //NavigationPage.SetHasBackButton(this, false);
-
+            NavigationPage.SetHasBackButton(this, false);
+            GetEmail.ReturnCommand = new Command(() => GetPassword.Focus());
+        }
+        string log;
+        private async void userIDCheck(object sender, EventArgs e)
+        {
+            UserDatabase userData = new UserDatabase();
+            if((string.IsNullOrWhiteSpace(GetEmail.Text)) || (string.IsNullOrWhiteSpace(GetPassword.Text)))
+            {
+                await DisplayAlert("Alert", "Enter Email", "OK");
+            }
+            else
+            {
+                log = GetEmail.Text;
+                var textresult = userData.updateUserValidation(GetEmail.Text);
+                if (textresult)
+                {
+                    await DisplayAlert("User Mail Id Not Exist", "Enter Correct User Name", "OK");
+                }
+            }
         }
 
         async void ForgotButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Recovery()); }
         async void RegButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Register()); }
-        async void SubmitButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new MainPage()); }
 
+        async void SubmitButtonClicked(object sender, EventArgs e) 
+        {
+            UserDatabase userData = new UserDatabase();
+
+            if (GetEmail.Text != null && GetPassword.Text != null)
+            {
+                var validData = userData.LoginValidate(GetEmail.Text, GetPassword.Text);
+                if (validData)
+                {
+                    await Navigation.PushAsync(new MainPage());
+                }
+                else
+                {
+                    await DisplayAlert("Login Failed", "Email or Password Incorrect", "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Oh no!", "Please enter Email & Password", "OK");
+            }
+        }
     }
 }
