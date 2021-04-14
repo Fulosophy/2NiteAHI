fcb56581a8f6df742204a902d8f5643cb3e31108
Mike <mcolandrea@student.fullsail.edu>
2021-04-14 13:13:49 -0400

    User Location Is now listed at the top of the Location List screen as a Town,PostalCode

--- 2NiteAHI/2NiteAHI/LocationList.xaml.cs
+++ 2NiteAHI/2NiteAHI/LocationList.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,13 +14,49 @@ namespace _2NiteAHI
     //Fixing an accidental merge sucks...
     public partial class LocationList : ContentPage
     {
+       
+
         public LocationList()
         {
             InitializeComponent();
+            BindingContext = this;
+            GetUserLoc();
+
+
+           
+
+
+
+
         }
         async private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Settings());        
+        }
+        async private void GetUserLoc()
+        {
+            // Grabbing User Location and Zip Code
+            var request = new GeolocationRequest(GeolocationAccuracy.High); // Requesting a location pull
+            var location = await Geolocation.GetLocationAsync(request);
+
+            var GetAddy = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude); // Grabbing the users location details as a placemark.
+            var addy = GetAddy?.FirstOrDefault();
+
+         
+            MyLocation = $"{addy.Locality},{addy.PostalCode}"; // Grabs the users current locations Address-Town and Address-PostalCode
+
+
+        }
+        private string myloc;
+        public string MyLocation
+        {
+            get { return myloc; }
+            set
+            {
+               
+                myloc = value;
+                OnPropertyChanged(nameof(MyLocation));
+            }
         }
     }
 }
