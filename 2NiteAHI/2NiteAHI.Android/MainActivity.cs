032509020988721b51a40c7901ecf7dedcfe9f94
Lordodson <65096932+Lordodson@users.noreply.github.com>
2021-04-23 18:43:36 -0600

    Added app tile setting for android home page. Used our Logo image.

--- 2NiteAHI/2NiteAHI.Android/MainActivity.cs
+++ 2NiteAHI/2NiteAHI.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace _2NiteAHI.Droid
 {
-    [Activity(Label = "_2NiteAHI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "_2NiteAHI", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
