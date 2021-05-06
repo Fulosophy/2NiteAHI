d6732932283625adc8aa11ce42b2042a673da461
Lordodson <65096932+Lordodson@users.noreply.github.com>
2021-05-05 18:30:40 -0600

    -Added a functional login system with register and recovery.

--- 2NiteAHI/2NiteAHI/Login.xaml.cs
+++ 2NiteAHI/2NiteAHI/Login.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,12 +8,26 @@ namespace _2NiteAHI
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class Login : ContentPage
 {
-    public Login()
-    {
-        InitializeComponent();
-    }
+        public string userSel, pwdSel;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
         async void ForgotButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Recovery()); }
         async void RegButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Register()); }
-        async void SubmitButtonClicked(object sender, EventArgs e) { await Navigation.PushAsync(new MainPage()); }
+        async void SubmitButtonClicked(object sender, EventArgs e) 
+        {
+            if (userSel != App.username) { await DisplayAlert("Username not found!", "Please try again!", "Ok"); }
+            else if (pwdSel != App.pwd)  { await DisplayAlert("Incorrect Password!", "Please try again!", "Ok"); }
+            else if (userSel != App.username && pwdSel != App.pwd) {await DisplayAlert("Please enter a password", "Please try again!", "Ok"); }
+            else { await Navigation.PushAsync(new LocationList()); }
+            GetUsername.Text = string.Empty;
+            GetPassword.Text = string.Empty;
+
+        }
+
+        void Entry_Username(object sender, TextChangedEventArgs e) { userSel = e.NewTextValue; }
+        void Entry_Password(object sender, TextChangedEventArgs e) { pwdSel = e.NewTextValue;  }
     }
 }
--- 2NiteAHI/2NiteAHI/Register.xaml.cs
+++ 2NiteAHI/2NiteAHI/Register.xaml.cs
@@ -10,13 +10,65 @@ using Xamarin.Forms.Xaml;
 namespace _2NiteAHI
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Register : ContentPage
+    public partial class Register : ContentPage
     {
+        //VARIABLES
+        public string repwd;
+        User user1;
+
         public Register()
         {
             InitializeComponent();
         }
-        async void RegSubmitClicked(object sender, EventArgs e) { await Navigation.PushAsync(new Login()); }
 
+        //BUTTONS
+        async void RegSubmitClicked(object sender, EventArgs e) 
+        {
+            if (repwd != App.pwd)
+            {
+                await DisplayAlert("Please enter the same password as above", " ", "Ok");
+            }
+            await Navigation.PushAsync(new Login());
+            
+        }
+
+        //ENTRIES
+        void Entry_Email(object sender, TextChangedEventArgs e)
+        {
+            App.email = e.NewTextValue;
+            
+        }
+        void Entry_Username(object sender, TextChangedEventArgs e)
+        {
+            App.username = e.NewTextValue;
+        }
+        void Entry_Password(object sender, TextChangedEventArgs e)
+        {
+            App.pwd = e.NewTextValue;
+        }
+        void Entry_RePassword(object sender, TextChangedEventArgs e)
+        {
+            repwd = e.NewTextValue;
+        }
+        void Entry_PhoneNumber(object sender, TextChangedEventArgs e)
+        {
+            App.phoneNumber = e.NewTextValue;
+
+        }
+    }
+    public class User
+    {
+        public string username { get; set; }
+        public string pwd { get; set; }
+        public string email { get; set; }
+        public string phnumber { get; set; }
+
+        public User(string _username, string _pwd, string _email, string _phnumber)
+        {
+            username = _username;
+            pwd = _pwd;
+            email = _email;
+            phnumber = _phnumber;
+        }
     }
 }
