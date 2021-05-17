ee561acbcf389011af8605e1890e49d99e197887
Lordodson <65096932+Lordodson@users.noreply.github.com>
2021-05-17 03:08:54 -0600

    Merge: Refactored Setting cs
    Removed unused settings. Merged Theme.

--- 2NiteAHI/2NiteAHI/Settings.xaml.cs
+++ 2NiteAHI/2NiteAHI/Settings.xaml.cs
@@ -1,56 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Xamarin.Essentials;
-using System.Threading;
-using Xamarin.Forms.Maps;
-using GoogleApi;
-using GoogleApi.Entities.Places;
 
 namespace _2NiteAHI
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Settings : ContentPage
     {
-        private List<int> RadialOpt = new List<int>
-        {
-            1000,
-            3000
-        };
-        private List<string> PlaceOpt = new List<string>
-        {
-            "Bar",
-            "Food"
-        };
-        
+        public string _Email { get; set; }
+        public string _Username { get; set; }
+        public string _PhoneNumber { get; set; }
+
         public Settings()
         {
-            
             InitializeComponent();
-            radiusPicker.ItemsSource = RadialOpt;
-            placePicker.ItemsSource = PlaceOpt;
-            
+            _Email = App.email;
+            _Username = App.username;
+            _PhoneNumber = App.phoneNumber;
+            BindingContext = this;
         }
-
-        private void GPSLOC_Toggled(object sender, ToggledEventArgs e)
+        public void NightTheme_Toggled(object sender, ToggledEventArgs e)
         {
-           
-            if (GPSLOC.IsToggled == false)
-            {
-               // logic here to disable GPS LOCATION
-            }
-        }
-
-        private void NightTheme_Toggled(object sender, ToggledEventArgs e)
-        {
-            if (NightTheme.IsToggled == false)
-            {
-                // logic here to change to NIGHTTIME THEME
-            }
+            if (e.Value){ App.Current.UserAppTheme = OSAppTheme.Dark;}
+            else { App.Current.UserAppTheme = OSAppTheme.Light; }
         }
     }
 }
